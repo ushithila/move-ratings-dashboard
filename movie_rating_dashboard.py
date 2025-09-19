62d9cedaf25a3288ecc0dae03ece2a75ec5b8258
Shithila Urshi <78228551+ushithila@users.noreply.github.com>
2025-09-19 18:49:24 -0400

    2

--- movie_rating_dashboard.py
+++ movie_rating_dashboard.py
@@ -10,4 +10,9 @@ df = pd.read_csv('data/movie_ratings.csv')
 st.title('Genres Rated')
 df["genres"] = df["genres"].str.split('|')
 df_explode = df.explode('genres')
-st.bar_chart(df_explode['genres'].value_counts().sort_index())
+st.bar_chart(df_explode['genres'].value_counts().sort_values(ascending=True))
+
+#2 Genres with the Highest Ratings 
+st.title('Genres with the Highest Ratings')
+genre_rating = df_explode.groupby('genres')['rating'].mean().reset_index().sort_values("rating", ascending=False)
+st.bar_chart(genre_rating.set_index('genres'))
