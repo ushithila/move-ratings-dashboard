8968a42208f3594051390ce49e091345730fddac
Shithila Urshi <78228551+ushithila@users.noreply.github.com>
2025-09-19 19:02:44 -0400

    3

--- movie_rating_dashboard.py
+++ movie_rating_dashboard.py
@@ -12,7 +12,18 @@ df["genres"] = df["genres"].str.split('|')
 df_explode = df.explode('genres')
 st.bar_chart(df_explode['genres'].value_counts().sort_values(ascending=True))
 
+'''
+chart, ax = plt.subplots()
+ax.pie(df_explode['genres'].value_counts(), labels=df_explode['genres'].value_counts().index, autopct='%1.1f%%', startangle=90)
+st.pyplot(chart)
+'''
+
 #2 Genres with the Highest Ratings 
 st.title('Genres with the Highest Ratings')
 genre_rating = df_explode.groupby('genres')['rating'].mean().reset_index().sort_values("rating", ascending=False)
 st.bar_chart(genre_rating.set_index('genres'))
+
+#3 Mean Rating per Year
+st.title('Mean Rating per Year')
+year_rating = df.groupby('year')['rating'].mean().reset_index().sort_values("year", ascending=False)
+st.line_chart(year_rating.set_index('year')['rating'])
